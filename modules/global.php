bbb7e577c1890dcf8384228401f946f679a44fba
DanDaSG <dandasg2901@gmail.com>
2024-09-13 22:25:17 -0500

    Update global.php

--- modules/global.php
+++ modules/global.php
@@ -1,5 +1,5 @@
 <?php
-$botToken = "5449842563:AAGgqf0HuPFEuNjIOB_zfUyyk4Wn2gARTVQ"; // Enter ur bot token
+$botToken = "7069082200:AAFNkKFir6Q0FKSS2dhj884-2u9Fj01hlJs"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
